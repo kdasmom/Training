fc5ce42246f24e3c9e1f5b1bdbb0b7e9a4639284
thomas_messier <thomas.messier@gmail.com>
2013-03-07 15:33:51 -0500

    Added documentation for NP.lib.core.Net and NP.lib.core.DBProvider

--- app/lib/core/DBProvider.js
+++ app/lib/core/DBProvider.js
@@ -1,19 +1,27 @@
 /**
- * A Provider implementation which saves and retrieves state via the database through an ajax call.
+ * A Provider implementation which saves state to the database via an ajax call.
  *
+ * @author Thomas Messier
  */
 Ext.define('NP.lib.core.DBProvider', {
     extend: 'Ext.state.Provider',
 
     requires: ['NP.lib.core.Config'],
 
-    constructor: function(config){
+    constructor: function(cfg){
+        // Call the abstract provider constructor, which will initiate some variables
         this.callParent(arguments);
+        // Populate the state object with data from the Config object
         this.state = NP.lib.core.Config.getUserSettings();
     },
 
+    /**
+     * Saves a name/value pair for state to the database
+     */
     set: function(name, value){
+        // Use the Config object to save the state in the database
         NP.lib.core.Config.saveUserSetting(name, value);
+        // Call the abstract provider's set() function to save the name/value pair in the state object
         this.callParent(arguments);
     }
 });
--- app/lib/core/Net.js
+++ app/lib/core/Net.js
@@ -1,3 +1,81 @@
+/**
+ * The Net class is used for operations that require communication with a server or another client
+ * 
+ * For now, the main aim of the class is to wrap Ajax calls to make them easy to use and conform to
+ * certain standards. This is accomplished via calls to the remoteCall() function. The function can
+ * be used to batch calls to multiple service functions in the backend into one single ajax request
+ * and handle the results individually, together, or both. Here's an example for a call to a single
+ * function:
+ *
+ *		NP.lib.core.Net.remoteCall({
+ *			requests: {
+ *				service: 'UserService', 				// This is the PHP service you want to call
+ *				action: 'save',							// This is the function to call on that service
+ *				fname : 'Thomas',						// This will be passed as an argument to the function
+ *				lname : 'Messier',						// This will be passed as an argument to the function
+ *				// This function gets called if the ajax request succeeds
+ *				success: function(result, deferred) {
+ *					deferred.resolve(result);
+ *				},
+ *				// This function gets called if the ajax request fails
+ *				failure: function(response, options, deferred) {
+ *					deferred.reject('There was an error saving the user');
+ *				}
+ *			}
+ *		});
+ *
+ * Here's an example with batched calls:
+ *
+ *		var promise = NP.lib.core.Net.remoteCall({
+ *			requests: [{
+ *				service: 'UserService', 
+ *				action : 'save',
+ *				fname  : 'Thomas',
+ *				lname  : 'Messier',
+ *				// This function gets called if the ajax request succeeds with only the result returned for this operation
+ *				success: function(result, deferred) {
+ *					Ext.log('Thomas saved');
+ *				},
+ *				// This function gets called if the ajax request fails with only the result returned for this operation
+ *				failure: function(response, options, deferred) {
+ *					Ext.log('Saving Thomas failed');
+ *				}
+ *			},{
+ *				service: 'UserService', 
+ *				action : 'save',
+ *				fname  : 'Sud',
+ *				lname  : 'Luthra',
+ *				success: function(result, deferred) {
+ *					Ext.log('Sud saved');
+ *				},
+ *				failure: function(response, options, deferred) {
+ *					Ext.log('Saving Sud failed');
+ *				}
+ *			}],
+ *			// Method to use for the ajax request, defaults to GET if not included
+ *          method : 'GET',
+ *			// This function gets called if the ajax request succeeds after all callbacks for individual requests have run
+ *			// The first parameter is an array with the results for all the operations above
+ *			success: function(results, deferred) {
+ *				// Look through results for each operation
+ *				for (var i=0; i<results.length; i++) {
+ *					// Do something here for each one of the ajax operations above
+ *					...	
+ *				}
+ *				deferred.resolve(results);
+ *			},
+ *			// This function gets called if the ajax request fails after all callbacks for individual requests have run
+ *			failure: function(response, options, deferred) {
+ *				deferred.reject('There was an error with one of the operations.');
+ *			}
+ *		});
+ *
+ * Note how this example has success and failure functions for both the individual functions and for the
+ * operation as a whole. This is not required, you can have as many or as few callbacks as you want. Also
+ * note how the function returns a Deft.Promise object that can be used if needed, otherwise it can be ignored.
+ *
+ * @author Thomas Messier
+ */
 Ext.define('NP.lib.core.Net', {
 	singleton: true,
 	
