028b0fcdbf9ba20a5ae63f8a460e3e2f908c4293
thomas_messier <thomas.messier@gmail.com>
2013-03-07 11:57:23 -0500

    Added missing documentation to PHP files and removed Pimple from the lib/util folder since it's now in the vendor folder

--- lib/core/db/Group.php
+++ lib/core/db/Group.php
@@ -1,16 +1,27 @@
 <?php
 namespace NP\core\db;
 
+/**
+ * This abstracts out a GROUP BY clause for a SQL statement
+ *
+ * @author Thomas Messier
+ */
 class Group implements SQLElement {
 	/**
 	 * @var string|array
 	 */
 	protected $group;
 
+	/**
+	 * @var string|array
+	 */
 	public function __construct($group) {
 		$this->group = $group;
 	}
 
+	/**
+	 * @return string String representation of the group clause in a SQL statement
+	 */
 	public function toString() {
 		if (is_string($this->group)) {
 			return $this->group;
--- lib/core/db/Order.php
+++ lib/core/db/Order.php
@@ -1,16 +1,27 @@
 <?php
 namespace NP\core\db;
 
+/**
+ * This abstracts out an ORDER BY clause for a SQL statement
+ *
+ * @author Thomas Messier
+ */
 class Order implements SQLElement {
 	/**
 	 * @var string|array
 	 */
 	protected $order;
 
+	/**
+	 * @param string|array $order
+	 */
 	public function __construct($order) {
 		$this->order = $order;
 	}
 
+	/**
+	 * @return string String representation of the order clause in a SQL statement
+	 */
 	public function toString() {
 		if (is_string($this->order)) {
 			return $this->order;
