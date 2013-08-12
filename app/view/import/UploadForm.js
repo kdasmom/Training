b74cbf1145712eac1e6da2316f9d9cf049a031ad
Aliaksandr Zubik <alexzub@tut.by>
2013-08-12 12:49:38 +0300

    Change Main and uploadForm

--- app/view/import/UploadForm.js
+++ app/view/import/UploadForm.js
@@ -6,40 +6,46 @@
 Ext.define('NP.view.import.UploadForm', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.import.uploadform',
-    requires: ['NP.lib.ui.Grid'],
+    
+    requires: ['NP.view.shared.button.upload'],
+    
     border: false,
+    bodyPadding: 8,
+
     initComponent: function() {
+        var bar = [
+            {xtype: 'shared.button.upload'}
+        ];
+
+        this.tbar = bar;
+        this.bbar = bar;
+
         this.items = [
-            {
-                xtype: 'panel',
+            {html: 'Please Note:', border: false},
+            {html: '<ul><li>This upload tool is for new ' + this.entityName + ' only. \n\
+                        Any changes to the existing ' + this.entityName + ' should be made directly in ' + this.sectionName + '. </li></ul>',
                 border: false,
+                bodyPadding: '8 0 8 50',
+            },
+            {html: '<p>Select a valid CSV file to upload:</p>', border: false, margin: '10 0 0 0'},
+            {
+                itemId: 'form_upload',
+                xtype: 'form',
+                autoScroll: true,
+                border: false,
+                bodyPadding: 8,
                 items: [
-                    {html: 'Please Note:', border: false},
-                    {html: '<ul><li>This upload tool is for new GL Codes only. \n\
-                                Any changes to the existing GL Codes should be made directly in GL Account Setup. </li></ul>',
-                        border: false,
-                        bodyPadding: '8 0 8 50',
-                    },
-                    {html: '<p>Select a valid CSV file to upload:</p>', border: false, margin: '10 0 0 0'},
                     {
-                        itemId: 'form_upload',
-                        xtype: 'form',
-                        autoScroll: true,
-                        border: false,
-                        bodyPadding: 8,
-                        items: [
-                            {
-                                xtype: 'filefield',
-                                name: 'file_upload_category',
-                                width: 400,
-                                hideLabel: true,
-                                allowBlank: false
-                            }
-                        ]
+                        xtype: 'filefield',
+                        name: this.fieldName,
+                        width: 400,
+                        hideLabel: true,
+                        allowBlank: false
                     }
                 ]
-            },
+            }
         ];
+
         this.callParent(arguments);
     }
 
