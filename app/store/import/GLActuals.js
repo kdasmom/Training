3f8719707f5ed3b5dafd4d1433d14a95f1b68055
Aliaksandr Zubik <alexzub@tut.by>
2013-08-15 01:02:41 +0300

    Added model, view, store, config for GLActuals.

---
+++ app/store/import/GLActuals.js
@@ -0,0 +1,10 @@
+/**
+ * Store for GLBudgets
+ *
+ * @author Aliaksandr Zubik
+ */
+Ext.define('NP.store.import.GLActuals', {
+    extend: 'NP.lib.data.Store',
+	
+    model: 'NP.model.import.GLActual'
+});
