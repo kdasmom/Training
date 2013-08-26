a2b75e528ee138525c647158a20317db206ef1bd
Aliaksandr Zubik <alexzub@tut.by>
2013-08-26 11:05:52 +0300

    added config, model, store for UnitType

---
+++ app/store/import/UnitTypes.js
@@ -0,0 +1,10 @@
+/**
+ * Store for UnitTypes
+ *
+ * @author Aliaksandr Zubik
+ */
+Ext.define('NP.store.import.UnitTypes', {
+    extend: 'NP.lib.data.Store',
+	
+    model: 'NP.model.import.UnitType'
+});
