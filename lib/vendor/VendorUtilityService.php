c2c0b7553153a15d25a5a1ab8502e549b4f9c50a
Yura Rodchyn <rodchyn@gmail.com>
2013-08-28 16:35:05 +0300

    Vendor Utility save fixed

--- lib/vendor/VendorUtilityService.php
+++ lib/vendor/VendorUtilityService.php
@@ -10,6 +10,9 @@
 namespace NP\vendor;
 
 
+use NP\core\db\Insert;
+use NP\core\db\Select;
+use NP\core\db\Update;
 use NP\system\BaseImportService;
 
 class VendorUtilityService extends BaseImportService
@@ -34,31 +37,31 @@ class VendorUtilityService extends BaseImportService
     public function save(\ArrayObject $row, $entityClass)
     {
         $vendorName = $row['Vendor_ID'];
-        $result = $this->adapter->query('SELECT vs.vendorsite_id FROM vendorsite vs INNER JOIN vendor v ON vs.vendor_id=v.vendor_id AND ltrim(rtrim(vs.vendorsite_id_alt))=ltrim(rtrim(?)) WHERE vs.approval_tracking_id=vs.vendorsite_id', array($vendorName));
+        $result = $this->gateway->adapter->query('SELECT vs.vendorsite_id FROM vendorsite vs INNER JOIN vendor v ON vs.vendor_id=v.vendor_id AND ltrim(rtrim(vs.vendorsite_id_alt))=ltrim(rtrim(?)) WHERE vs.approval_tracking_id=vs.vendorsite_id', array($vendorName));
         $vendorId = !empty($result[0])?$result[0]['vendorsite_id']:null;
 
-        $utilityName = $row['Utility_Type'];
-        $result = $this->adapter->query('SELECT utilitytype_id FROM utilitytype WHERE utilitytype = ?', array($utilityName));
+        $utilityTypeName = $row['Utility_Type'];
+        $result = $this->gateway->adapter->query('SELECT utilitytype_id FROM utilitytype WHERE utilitytype = ?', array($utilityTypeName));
         $utilityTypeId = !empty($result[0])?$result[0]['utilitytype_id']:null;
 
         $glaccountName = $row['GL_Account'];
-        $result = $this->adapter->query('SELECT glaccount_id FROM glaccount	WHERE ltrim(rtrim(glaccount_number))=ltrim(rtrim(?))', array($glaccountName));
+        $result = $this->gateway->adapter->query('SELECT glaccount_id FROM glaccount	WHERE ltrim(rtrim(glaccount_number))=ltrim(rtrim(?))', array($glaccountName));
         $glaccountId = !empty($result[0])?$result[0]['glaccount_id']:null;
 
         $propertyName = $row['Property_ID'];
-        $result = $this->adapter->query('SELECT property_id FROM property WHERE ltrim(rtrim(property_id_alt))=ltrim(rtrim(?))', array($propertyName));
+        $result = $this->gateway->adapter->query('SELECT property_id FROM property WHERE ltrim(rtrim(property_id_alt))=ltrim(rtrim(?))', array($propertyName));
         $propertyId = !empty($result[0])?$result[0]['property_id']:null;
 
         $unitName = $row['Unit_Id'];
-        $result = $this->adapter->query('SELECT unit_id From unit WHERE ltrim(rtrim(unit_id_alt))=ltrim(rtrim(?)) AND property_id = ?', array($unitName, $propertyId));
+        $result = $this->gateway->adapter->query('SELECT unit_id From unit WHERE ltrim(rtrim(unit_id_alt))=ltrim(rtrim(?)) AND property_id = ?', array($unitName, $propertyId));
         $unitId = !empty($result[0])?$result[0]['unit_id']:null;
 
         $entityData = array(
             'vendor_id_alt' => $vendorName,
             'vendorsite_id' => $vendorId,
-            'utility_type' => $utilityName,
+            'utility_type' => $utilityTypeName,
             'utilitytype_id' => $utilityTypeId,
-            'account_number' => $glaccountName,
+            'account_number' => $row['Account_Number'],
             'glaccount_id' => $glaccountId,
             'userprofile_id'=> $this->securityService->getUserId(),
             'property_code' => $propertyName,
@@ -72,14 +75,63 @@ class VendorUtilityService extends BaseImportService
         $entity = new $entityClass($entityData);
         $errors = $this->validate($entity);
 
+
+
+
         // If the data is valid, save it
         if (count($errors) == 0) {
             // Begin transaction
+
             $this->gateway->beginTransaction();
 
             try {
-                // Save the glaccount record
-                $this->gateway->save($entity);
+                $select = new Select('utility');
+                $select->column('utility_id')
+                    ->where('utilitytype_id = ? and vendorsite_id = ?');
+
+                $result = $this->gateway->adapter->query($select, array($utilityTypeId, $vendorId));
+                $utilityId = !empty($result[0])?$result[0]['utility_id']:null;
+
+                if(empty($result)) {
+                    $insert = new Insert();
+                    $insert->into('Utility')
+                        ->columns(array('UtilityType_Id', 'Property_Id', 'Vendorsite_Id'))
+                        ->values(array($utilityTypeId, $propertyId, $vendorId));
+
+                    $this->gateway->adapter->query($insert);
+
+                    $update = new Update('UtilityAccount');
+                    $update->value('glaccount_id', $glaccountId);
+                    $update->value('unit_id', $unitId);
+                    $update->where('property_id = ? AND UtilityAccount_AccountNumber = ? AND UtilityAccount_MeterSize = ?');
+
+                    $this->gateway->adapter->query($update, array($propertyId, $row['Account_Number'], $row['Meter_Number']));
+                } else {
+
+                    $select = new Select('utilityaccount');
+                    $select->where('Utility_Id = ? AND UtilityAccount_AccountNumber = ? AND property_id = ? AND UtilityAccount_MeterSize = ?');
+                    $selectResult = $this->gateway->adapter->query($select, array($utilityId, $row['Account_Number'], $propertyId, $row['Meter_Number']));
+
+                    if(empty($selectResult)) {
+                        $insert = new Insert('UtilityAccount');
+                        $insert->columns(array('Utility_Id', 'UtilityAccount_AccountNumber', 'property_id', 'glaccount_id', 'unit_id', 'UtilityAccount_MeterSize', 'utilityaccount_active'));
+                        $insert->values(array($utilityId, $row['Account_Number'], $propertyId, $glaccountId, $unitId, $row['Meter_Number'], 1));
+                    }
+                }
+
+                $select = new Select('vendorglaccounts');
+                $select->where('vendor_id = ? AND glaccount_id = ?');
+                $vendorGLAccountsSelect = $this->gateway->adapter->query($select, array($vendorId, $glaccountId));
+
+                if(empty($vendorGLAccountsSelect)) {
+                    $insert = new Insert('vendorglaccounts');
+                    $insert->columns(array('vendor_id', 'glaccount_id'));
+                    $insert->values(array($vendorId, $glaccountId));
+
+                    $this->gateway->adapter->query($select, array($vendorId, $glaccountId));
+                }
+
+
             } catch(\Exception $e) {
                 // Add a global error to the error array
                 $errors[] = array('field' => 'global', 'msg' => $this->handleUnexpectedError($e), 'extra'=>null);
