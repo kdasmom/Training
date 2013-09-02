7dd19e3d3ef388c9af2fd15b9bb12c8fe0ed754d
Yura Rodchyn <rodchyn@gmail.com>
2013-09-02 17:56:25 +0300

    vendorGL

--- lib/vendor/VendorGLService.php
+++ lib/vendor/VendorGLService.php
@@ -16,14 +16,41 @@ class VendorGLService extends BaseImportService
 {
 
     /**
+     * @var VendorGLGateway
+     */
+    protected $gateway;
+
+    public function __construct(VendorGLGateway $gateway)
+    {
+        $this->gateway = $gateway;
+    }
+    /**
      * This must be implemented in child class.
      * Method accept row and entity class to save in related gateway.
      *
      * @param \ArrayObject $data Row array for entity defined in next param
      * @param string $entityClass Entity class to map data
      */
-    public function save(\ArrayObject $data, $entityClass)
+    public function save(\ArrayObject $row, $entityClass)
     {
-        // TODO: Implement save() method.
+        $vendorName = $row['VendorCode'];
+        $glCodes = $row['GLCodes'];
+        $integrationPackageName = $row['Integration Package'];
+
+        $query = "SELECT integration_package_id FROM integrationpackage WHERE integration_package_name = ?";
+        $result = $this->gateway->adapter->query($query, array($integrationPackageName));
+        $integrationPackageId = !empty($result[0])?$result[0]['integration_package_id']:null;
+
+        $query = "SELECT vendor_id FROM vendor WHERE vendor_id_alt = ? AND integration_package_id = ?";
+        $result = $this->gateway->adapter->query($query, array($vendorName, $integrationPackageId));
+        $vendorId = !empty($result[0])?$result[0]['vendor_id']:null;
+
+        $query = "SELECT glaccount_id FROM glaccount WHERE glaccount_number = ?";
+        $result = $this->gateway->adapter->query($query, array($glCodes));
+        $glaccountId = !empty($result[0])?$result[0]['glaccount_id']:null;
+
+        $query = "INSERT INTO VENDORGLACCOUNTS(vendor_id, glaccount_id) VALUES(?, ?)";
+        $result = $this->gateway->adapter->query($query, array($vendorId, $glaccountId));
+
     }
 }
