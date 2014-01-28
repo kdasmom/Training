268b228f0c11822d09562442334156942bf0f220
thomas_messier <thomas.messier@gmail.com>
2014-01-28 12:10:56 -0500

    17528 - fixed migration that was causing errors

--- migrations/20140113134500_change_module_name.php
+++ migrations/20140113134500_change_module_name.php
@@ -8,30 +8,18 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class ModifyModuleName extends AbstractMigration
+class ChangeModuleName extends AbstractMigration
 {
 	/**
 	 * Migrate Up.
 	 */
 	public function up()
 	{
-		$select = new \NP\core\db\Select();
-		$parentSelect = new \NP\core\db\Select();
-
-		$select->from(['m' => 'module'])
-				->column('module_id')
-				->where(['module_name', '?']);
-
-		$module_id = $this->adapter->query($select, ['Vendor Portal']);
-
-		$parentSelect->from(['t' => 'tree'])
-					->column('tree_id')
-					->where(['table_name' => 'module', 'tablekey_id' => '?']);
-
-		$parent_id = $this->adapter->query($parentSelect, [$module_id]);
-
-		$this->execute("UPDATE module set module_name = 'VendorConnect' where module_id = {$module_id}");
-		$this->execute("update module set module_name = REPLACE(module_name, 'Vendor Portal', 'VendorConnect') where module_id in (select tablekey_id from tree t where t.tree_parent = {$parent_id})");
+		$this->execute("
+			UPDATE module SET
+			module_name = REPLACE(module_name, 'Vendor Portal', 'VendorConnect')
+			WHERE module_name LIKE '%Vendor Portal%'
+		");
 	}
 
 	/**
@@ -39,22 +27,10 @@ class ModifyModuleName extends AbstractMigration
 	 */
 	public function down()
 	{
-		$select = new \NP\core\db\Select();
-		$parentSelect = new \NP\core\db\Select();
-
-		$select->from(['m' => 'module'])
-			->column('module_id')
-			->where(['module_name', '?']);
-
-		$module_id = $this->adapter->query($select, ['VendorConnect']);
-
-		$parentSelect->from(['t' => 'tree'])
-			->column('tree_id')
-			->where(['table_name' => 'module', 'tablekey_id' => '?']);
-
-		$parent_id = $this->adapter->query($parentSelect, [$module_id]);
-
-		$this->execute("UPDATE module set module_name = 'VendorConnect' where module_id = {$module_id}");
-		$this->execute("update module set module_name = REPLACE(module_name, 'VendorConnect', 'Vendor Portal') where module_id in (select tablekey_id from tree t where t.tree_parent = {$parent_id})");
+		$this->execute("
+			UPDATE module SET
+			module_name = REPLACE(module_name, 'VendorConnect', 'Vendor Portal')
+			WHERE module_name LIKE '%VendorConnect%'
+		");
 	}
 }
