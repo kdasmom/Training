a0156a2d4e2e8cc5857d37a45b81661a6019cd68
Andrey Baranov <randebar@gmail.com>
2014-04-29 14:42:49 +0400

    NXS-50 Need to add Dashboard definition to Overview page. Add Dashboard definitions

--- app/view/mySettings/Overview.js
+++ app/view/mySettings/Overview.js
@@ -22,6 +22,7 @@ Ext.define('NP.view.mySettings.Overview', {
 		me.introText          = 'My Settings allows users to change their personal user information as well as manage certain system settings. The following tabs contain information to allow the user to manage the following information:';
 		me.userInfoText       = '<b>User Information</b> - allows the user to change their password and update personal information such as their contact information and email address.';
 		me.settingsText       = '<b>Settings</b> - allows the user to configure their dashboard default settings of what {property} they want to automatically log into as well as what summary statistic they want to auto display.';
+		me.dashboardText      = '<b>Dashboard</b> - allows the user to customize the dashboard page.';
 		me.displayText        = '<b>Display</b> - allows the user to choose which default percentage setting they want to view on split screen views.  Note this is only applicable for users with access to view images in the system.';
 		me.emailNotifText     = '<b>Email Notification</b>- allows the user to manage specific email alert frequencies for Purchase Orders and/or Invoices that require approval and for budget overage notifications.';
 		me.mobileSettingsText = '<b>Mobile Settings</b>- allows users to register their mobile phone to begin using the Mobile Application for PO; Receipt and Invoice approvals. If you do not see this tab display; your role right has not been granted access to this section.';
@@ -32,7 +33,8 @@ Ext.define('NP.view.mySettings.Overview', {
 	    	'<br /><br />' +
 			'<ul>' +
 				'<li>' + NP.Translator.translate(me.userInfoText) +'</li>' +
-				'<li>' + NP.Translator.translate(me.settingsText, { property: NP.Config.getPropertyLabel() }) +'</li>';
+				'<li>' + NP.Translator.translate(me.settingsText, { property: NP.Config.getPropertyLabel() }) +'</li>' +
+				'<li>' + NP.Translator.translate(me.dashboardText) +'</li>';
 
 		var settingVal = NP.lib.core.Config.getSetting('pn.main.WebDocumentz', 0);
 		if (settingVal == 1 || settingVal == 2) {
